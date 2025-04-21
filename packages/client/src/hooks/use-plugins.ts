36af6f2f85db55e338fef76c1403c81040818f81
Avan <khanhnc@cyberk.io>
2025-04-21 19:18:53 +0700

    add plugin-openrouter

--- packages/client/src/hooks/use-plugins.ts
+++ packages/client/src/hooks/use-plugins.ts
@@ -16,6 +16,7 @@ export function usePlugins() {
 
       // Temporarily return hardcoded plugins as an array
       return [
+        '@elizaos/plugin-openrouter',
         '@elizaos/plugin-bootstrap',
         '@elizaos/plugin-sql',
         '@elizaos/plugin-twitter',
